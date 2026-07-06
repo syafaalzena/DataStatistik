2e2465ea08f3e878e3582c5b0f89bdfc85377f4d
qurrotaaini29 <qurrotaaini03738@gmail.com>
2026-07-06 08:50:58 +0700

    up remember me

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,15 +41,15 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
+    if (! Auth::attempt($this->only('email', 'password'), true)) {
+        RateLimiter::hit($this->throttleKey());
 
-            throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
-            ]);
-        }
+        throw ValidationException::withMessages([
+            'email' => trans('auth.failed'),
+        ]);
+    }
 
-        RateLimiter::clear($this->throttleKey());
+    RateLimiter::clear($this->throttleKey());
     }
 
     /**
