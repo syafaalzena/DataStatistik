77d9d45f915ac9f57cf28c62e04fa5421ee4c7cf
qurrotaaini29 <qurrotaaini03738@gmail.com>
2026-07-08 10:21:44 +0700

    up mcr

---
+++ app/Http/Controllers/KabupatenIkanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KabupatenIkan;
+use Illuminate\Http\Request;
+
+class KabupatenIkanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KabupatenIkan $kabupatenIkan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KabupatenIkan $kabupatenIkan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KabupatenIkan $kabupatenIkan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KabupatenIkan $kabupatenIkan)
+    {
+        //
+    }
+}
